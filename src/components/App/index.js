188af07c32dd5d3e56cfc3ead8e2ac6cfac44992
ThomasLutaster <lutaster.t@gmail.com>
2021-01-19 15:36:41 +0100

    component Event created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Review from 'src/components/Review';
 import Button from 'src/components/Button';
 import Reviews from 'src/components/Reviews';
+import Event from 'src/components/Event';
 
 import './styles.scss';
 
@@ -14,6 +15,7 @@ const App = () => (
     <Review />
     <Button />
     <Reviews />
+    <Event />
   </div>
 );
 
