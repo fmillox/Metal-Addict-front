9d8389f3366fc0ce9e88174544d92eae93f9b612
ThomasLutaster <lutaster.t@gmail.com>
2021-01-19 14:58:05 +0100

    test creation reviews

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 // == Import
 import Review from 'src/components/Review';
-
+import Reviews from 'src/components/Reviews';
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Review />
+    <Reviews />
   </div>
 );
 
