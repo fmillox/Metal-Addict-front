2e80edd23e36d470c76abf3cba92182def67d589
ThomasLutaster <lutaster.t@gmail.com>
2021-01-19 14:14:06 +0100

    installation du modele

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    
+  </div>
+);
+
+// == Export
+export default App;
