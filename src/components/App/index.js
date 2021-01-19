8504e2e181c93c957728384f3ca6debf4c4765dc
ThomasLutaster <lutaster.t@gmail.com>
2021-01-19 14:30:34 +0100

    création du composant Review pour test

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@
 import React from 'react';
 
 // == Import
+import Review from 'src/components/Review';
 
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    
+    <Review />
   </div>
 );
 
---
+++ src/components/Review/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './review.scss';
+
+const Review = () => (
+  <div>Hello Review</div>
+);
+
+export default Review;
