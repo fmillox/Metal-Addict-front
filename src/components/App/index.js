bf6008b35a405c31792ee06264aec5e2ea11f178
ThomasLutaster <lutaster.t@gmail.com>
2021-01-20 12:05:05 +0100

    test fictive roads

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,25 @@
 // == Import npm
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 // == Import
 
 import Main from 'src/components/Main';
+import Header from 'src/components/Header';
 
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Main />
+    <Switch>
+      <Route exact path="/header">
+        <Header />
+      </Route>
+      <Route>
+        <Main />
+      </Route>
+    </Switch>
   </div>
 );
 
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from 'src/components/Home';
+import Footer from 'src/components/Footer';
 
 import './main.scss';
 
 const Main = () => (
   <div className="main">
-    <Home />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/footer">
+        <Footer />
+      </Route>
+      <Route>
+        <div>404</div>
+      </Route>
+    </Switch>
   </div>
 );
 
