bd233dfdfede81dd2ef249bbcf189e1d6a4964df
ThomasLutaster <lutaster.t@gmail.com>
2021-01-20 11:42:00 +0100

    form added in homepage and component Main created

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Home from 'src/components/Home';
+
+import './main.scss';
+
+const Main = () => (
+  <div className="main">
+    <Home />
+  </div>
+);
+
+export default Main;
