fdaec0b942dd59ed63ff2cd543e9e35f67c77807
ThomasLutaster <lutaster.t@gmail.com>
2021-01-22 12:25:42 +0100

    smallreview ok with back

---
+++ src/components/Reviews/SmallReview.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './reviews.scss';
+
+import dave from 'src/images/dave.jpg';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+const SmallReview = ({
+  title,
+  createdAt,
+  event,
+}) => (
+  <article
+    className="smallReview"
+    data-aos="zoom-in"
+    data-aos-easing="linear"
+    data-aos-duration="1000"
+  >
+    <div className="avatar"><img src={dave} className="avatar-image" alt="" /></div>
+    <div className="content">
+      <h3 className="title">{title} </h3>
+      <p className="date">posté le {createdAt}</p>
+      <p className="name">{event.band.name}</p>
+      <p className="venue">{event.venue} - {event.city} - {event.country.name}</p>
+    </div>
+  </article>
+);
+
+SmallReview.propTypes = {
+  title: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  event: PropTypes.shape({
+    venue: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    band: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }.isRequired).isRequired,
+  }.isRequired).isRequired,
+};
+
+export default SmallReview;
--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './reviews.scss';
 
-import SmallReview from 'src/components/SmallReview';
+import SmallReview from 'src/components/Reviews/SmallReview';
 
 const Reviews = ({ reviews }) => (
   <div className="reviews">
+    {console.log(reviews)};
     {reviews.map((review) => (
       <SmallReview key={review.id} {...review} />
     ))}
-
-    <SmallReview />
-    <SmallReview />
-    <SmallReview />
-    <SmallReview />
   </div>
 );
+
+Reviews.propTypes = {
+  reviews: PropTypes.array.isRequired,
+};
 
 export default Reviews;
