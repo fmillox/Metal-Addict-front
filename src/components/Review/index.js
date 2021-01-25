ff77903a7393d29a813e31d0b48c27a123790efb
ThomasLutaster <lutaster.t@gmail.com>
2021-01-25 15:13:04 +0100

    component Review created - css and manage pictures to do

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,11 +1,102 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import {
+  useParams,
+  Link,
+} from 'react-router-dom';
+
+import Pictures from 'src/components/Pictures';
+
+import ScaleLoader from 'react-spinners/ScaleLoader';
+
+import { getIdFromSlug } from 'src/utils';
+
+import dave from 'src/images/dave.jpg';
+import Moment from 'moment';
 
 import './review.scss';
 
-const Review = () => (
-  <div className="review">
-    review
-  </div>
-);
+const Review = ({
+  loadReview,
+  loadingReview,
+  review,
+  picture,
+  pictures,
+  loadingPictures,
+}) => {
+  const { slug } = useParams();
+  const id = getIdFromSlug(slug);
+  const refReview = useRef(null);
+
+  useEffect(() => {
+    loadReview(id);
+    refReview.current.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, []);
+
+  return (
+    <div className="review" ref={refReview}>
+      {
+        loadingReview && <ScaleLoader />
+      }
+
+      {
+      !loadingReview && (
+        <>
+          <Link
+            className="review-back-to-reviews-results"
+            to="/"
+          >
+            Retour à la page précédente
+          </Link>
+          <div className="review-title">
+            {review.title}
+          </div>
+          <img src={dave} alt="" className="image" />
+          <div>{review.user.nickname}{Moment(review.createdAt).locale('fr').format('L')}</div>
+          <img src={picture} alt="" className="image2" />
+          <div>{review.content}</div>
+          {
+            loadingPictures && <ScaleLoader />
+          }
+          {
+            !loadingPictures && (
+              <div className="event-pictures-container">
+                <div className="event-pictures-label">
+                  Photos
+                </div>
+                <div className="event-pictures-list">
+                  <Pictures pictures={pictures} />
+                </div>
+              </div>
+            )
+          }
+        </>
+      )
+      }
+    </div>
+  );
+};
+
+Review.propTypes = {
+  loadingReview: PropTypes.bool.isRequired,
+  loadReview: PropTypes.func.isRequired,
+  picture: PropTypes.string.isRequired,
+  loadingPictures: PropTypes.bool.isRequired,
+  pictures: PropTypes.arrayOf(PropTypes.object).isRequired,
+  review: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      nickname: PropTypes.string.isRequired,
+    }.isRequired),
+  }.isRequired),
+};
+Review.defaultProps = {
+  review: null,
+};
 
 export default Review;
--- src/components/Reviews/SmallReview.js
+++ src/components/Reviews/SmallReview.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import './reviews.scss';
+
+import { getSlug } from 'src/utils';
 
 import dave from 'src/images/dave.jpg';
 import Moment from 'moment';
@@ -11,28 +14,49 @@ import 'aos/dist/aos.css';
 AOS.init();
 
 const SmallReview = ({
+  id,
   title,
   createdAt,
   event,
-}) => (
-  <article
-    className="smallReview"
-    data-aos="flip-down"
-    data-aos-easing="linear"
-    data-aos-duration="500"
-  >
-    <div className="avatar"><img src={dave} className="avatar-image" alt="" /></div>
-    <div className="content">
-      <h3 className="title">{title} </h3>
-      <p className="date">posté le {Moment(createdAt).locale('fr').format('L')}
-      </p>
-      <p className="name">{event.band.name}</p>
-      <p className="venue">{event.venue} - {event.city} - {event.country.name}</p>
-    </div>
-  </article>
-);
+  setBackLink,
+}) => {
+  // eslint-disable-next-line prefer-template
+  const nextLocation = '/chronique/' + getSlug(event.band.name, id);
+  const history = useHistory();
+  const currentLocation = useLocation();
+
+  const handleOnClick = () => {
+    setBackLink(currentLocation);
+    // console.log(currentLocation);
+    history.push(nextLocation);
+  };
+
+  return (
+    <article
+      className="smallReview"
+      data-aos="flip-down"
+      data-aos-easing="linear"
+      data-aos-duration="500"
+    >
+      <div className="avatar"><img src={dave} className="avatar-image" alt="" /></div>
+      <a
+        className="title"
+        onClick={handleOnClick}
+      >
+        <div className="content">
+          <h3 className="title">{title} </h3>
+          <p className="date">posté le {Moment(createdAt).locale('fr').format('L')}
+          </p>
+          <p className="name">{event.band.name}</p>
+          <p className="venue">{event.venue} - {event.city} - {event.country.name}</p>
+        </div>
+      </a>
+    </article>
+  );
+};
 
 SmallReview.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
   event: PropTypes.shape({
@@ -43,6 +67,7 @@ SmallReview.propTypes = {
       name: PropTypes.string.isRequired,
     }.isRequired).isRequired,
   }.isRequired).isRequired,
+  setBackLink: PropTypes.func.isRequired,
 };
 
 export default SmallReview;
