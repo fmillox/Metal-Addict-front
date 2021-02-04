fcd01f5d1bb9f86651d2a8d678ed5d34fba5c2c2
ThomasLutaster <lutaster.t@gmail.com>
2021-02-04 19:03:35 +0100

    not found page done / introduction in homepage done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 
 import './home.scss';
 
-import { ArrowLeft, ArrowRight } from 'react-feather';
 import { ScaleLoader } from 'react-spinners';
 
-import firstPicture from 'src/images/concert8.jpg';
-import secondPicture from 'src/images/concert13.jpg';
+import firstPicture from 'src/assets/images/concert8.jpg';
+import secondPicture from 'src/assets/images/concert13.jpg';
 
 import Reviews from 'src/components/Reviews';
 
@@ -29,30 +28,52 @@ const Home = ({
 
   return (
     <div className="home">
-      <div className="news">
-        <ArrowLeft />
-        <p className="news-content">Nouvelle fonctionnalité : ajouter vos photos!!</p>
-        <ArrowRight />
+      <div className="introduction">
+        <div className="introduction-content">
+          <h2 className="introduction-content-title">Welcome dear headbanger !</h2>
+          <p className="introduction-content-text">
+            Ici tu peux répertorier  les concerts inoubliable auxquels tu as assistés et partager ton expérience !<br />Ou tout simplement chercher où a tourné ce groupe underground moldave des années 90 dont tout le monde parle (ou pas).
+          </p>
+          <p className="introduction-content-register">
+            Pour t’inscrire ça se passe <span className="introduction-content-register-link">ici</span>
+          </p>
+        </div>
       </div>
 
       <div className="firstBlock">
-        <div className="introduction" data-aos="fade-up-bottom">
-          <p>Revivez vos meilleurs concerts grâce aux photos et reviews postées.</p>
-          <p>Inscrivez-vous pour nous partarger vos meilleurs souvenirs.</p>
-        </div>
-        <div className="firstImage" data-aos="fade-up-top" data-aos-duration="1000">
+      <div className="firstImage" data-aos="fade-up-top" data-aos-duration="1000">
           <img src={firstPicture} className="image" alt="" />
         </div>
+        <div className="description" data-aos="fade-up-bottom">
+          <p>
+            Tu peux désormais partager tes photos de concert avec les autres metalheads ! Alors rend-toi tout de suite sur la page de ton dernier concert pour y poster la photo de ton pote qui a encore trop bu et qui est monté sur scène piquer le micro… (on le connait que trop bien)
+          </p>
+          <p>Inscrivez-vous pour nous partarger vos meilleurs souvenirs.</p>
+        </div>
+        
       </div>
-
-      <div className="description"> <p data-aos="fade-left" data-aos-duration="500">Quis omnis cum ut veritatis sit iste architecto aut libero ipsum repellendus</p></div>
 
       <h2 className="lastReviews">Dernières reviews</h2>
       {loadingReviews && <ScaleLoader color={SECONDARY_COLOR} />}
       {!loadingReviews && <Reviews reviews={reviews} />}
 
       <div className="secondBlock">
-        <div className="about" data-aos="fade-down-top" data-aos-duration="1000">About</div>
+        <div className="about" data-aos="fade-down-top" data-aos-duration="1000">
+          <p>
+            Ce site est l’aboutissement du projet d’apothéose d’Oclock (promo Lyra-PHP) dont l’idée à germée dans la tête de Thomas. Il a rapidement été rejoint par Frédéric, Hugo et … Hugo.
+          </p>
+          <p>
+            La team :
+            Thomas Lutaster (dév. Front)
+            Frédéric Millox (dév. Front)
+            Hugo Roy (dév. Back)
+            Hugo Drelon (dév. Back)
+          </p>
+          <p>
+            Topo technique :
+            Ce site a été développé en React et Symfony. Les données (groupes, concerts, images…) ont été rendue accessibles grâce à l’utilisation des APIs de Musicbrainz.org, Setlist.fm et Fanart.tv.
+          </p>
+        </div>
         <div className="secondImage" data-aos="zoom-out-bottom" data-aos-duration="1500">
           <img src={secondPicture} className="image" alt="" />
         </div>
